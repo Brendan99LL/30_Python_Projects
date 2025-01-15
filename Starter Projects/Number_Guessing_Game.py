73dc6ae33689aef9aa54ed9c7c35e6798182910c
Brendan99LL <brendan.lapidus@gmail.com>
2025-01-15 10:45:38 -0500

    commit

--- Starter Projects/Number_Guessing_Game.py
+++ Starter Projects/Number_Guessing_Game.py
@@ -6,18 +6,22 @@ lower_num, higher_num = 1, 10
 random_number: int = randint(lower_num, higher_num)
 print(f'Guess the number in the range from {lower_num} to {higher_num}.')
 
+# Make sure that the program will work only if the user inputs a number
+# Print a statement that would prevent the user from inputing something besides a number
 while True:
     try:
         user_guess: int = int(input('Guess: '))
     except ValueError as e:
         print('Please enter a valid number.')
         continue
-
+    
+    # Print statements if the number inputed is lower, higher, and when it is the same as the random number
     if user_guess > random_number:
         print('The number is lower.')
     elif user_guess < random_number:
         print('The number is higher.')
     else:
         print('You guessed it!')
+        # Finish the program
         break
     
